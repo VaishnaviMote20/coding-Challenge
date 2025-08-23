217b27ac0a66ca701b703c5a9ae51b1fbbd46638
Vaishnavi <vaishnavim205@gmail.com>
2025-08-23 17:15:21 +0530

    Create Day9.py
    Day9:- Longest common prefix

---
+++ Day9.py
@@ -0,0 +1,19 @@
+def longestCommonPrefix(strs):
+    if not strs:  
+        return ""
+
+    prefix = strs[0]
+
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    return prefix
+
+print(longestCommonPrefix(["flower", "flow", "flight"]))
+print(longestCommonPrefix(["dog", "racecar", "car"]))
+print(longestCommonPrefix(["apple", "ape", "april"]))
+print(longestCommonPrefix([""]))
+print(longestCommonPrefix(["alone"]))
+print(longestCommonPrefix([]))
